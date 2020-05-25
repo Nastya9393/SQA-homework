9cbbc1917325be6a13eeecdd9e32bd3a13f96944
zvezdoczet <59225573+zvezdoczet@users.noreply.github.com>
2020-05-25 13:48:48 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,18 @@
+package Task;
+
+import java.util.Scanner;
+
+ // к заданию
+		public class Task1 {
+			public static void main(String[] args){
+				System.out.print("Введите натуральное число X:\t");
+				Scanner sc = new Scanner(System.in);
+				int x = sc.nextInt(); 
+				System.out.print("введите натуральное число Y:\t");
+				int y = sc.nextInt();
+				sc.close();
+				System.out.println("\nРезультаты =>\n\tцелоцисленное деление X на Y:\t" + x/y);
+				System.out.println("\tостаток от деления X на Y:\t" + x%y);
+				System.out.println("\tквадратный корень X:\t\t" + Math.sqrt(x) + "\n\n! Вычисления закончены !");
+			}
+}
