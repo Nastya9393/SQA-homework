0be735b790f893d9c2cbdb6e3703de9f851d99d9
zvezdoczet <59225573+zvezdoczet@users.noreply.github.com>
2020-05-25 16:40:39 +0300

    Add files via upload

--- Task1.java
+++ Task1.java
@@ -2,10 +2,12 @@ package Task3_1;
 // Написать программу, которая предлагает пользователю ввести c клавиатуры номер дня недели, и в ответ 
 // показывает название этого дня (например, 6 – это суббота). Решить с использованием switch.
 
+import java.util.Scanner;
+
 public class Task1 {
 
 	public static void main(String[] args) {
-	
+		System.out.print("Введите число дня недели: ");
 
 	}
 
