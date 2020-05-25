daec04f21138e099dfe1e658894cb1c0ee9a318a
zvezdoczet <59225573+zvezdoczet@users.noreply.github.com>
2020-05-25 17:57:50 +0300

    Add files via upload

--- Task1.java
+++ Task1.java
@@ -10,8 +10,33 @@ public class Task1 {
 		System.out.print("Введите число дня недели: ");
 		Scanner sc = new Scanner(System.in);
 		int nd = sc.nextInt();
-		sc.close();
-		System.out.print("Это будет " + nd);
+		sc.close(); 
+		String dw = " нет такого дня ";
+		switch (nd) {
+		case 1:
+			dw = " понедельник / Monday ";
+		break;
+		case 2:
+			dw = " вторник / Tuesday ";
+		break;
+		case 3:
+			dw = " среда / Wednesday ";
+		break;
+		case 4:
+			dw = " четверг / Thurday ";
+		break;
+		case 5:
+			dw = " пятница / Friday ";
+		break;
+		case 6:
+			dw = " суббота / Saturday ";
+		break;
+		case 7:
+			dw = " воскресенье / Sunday ";
+		break;
+		}
+		System.out.print(nd + " Это будет => " + dw);
 	}
-
 }
+		
+	
