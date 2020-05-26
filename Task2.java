f05ce2da4f099a95482330b75c7f3cd1420c86c1
zvezdoczet <59225573+zvezdoczet@users.noreply.github.com>
2020-05-26 19:56:36 +0300

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,22 @@
+package Task;
+
+//В переменной n хранится натуральное (целое) трехзначное число. Создайте программу, 
+//вычисляющую и выводящую на экран сумму цифр числа n.
+
+import java.util.Scanner;
+
+public class Task2 {
+	public static void main (String[] args) {
+		System.out.print("Введите натуральное трехзначное число X:\t");
+		Scanner sc = new Scanner(System.in); 
+		int x;
+			do {									//проверка на трехзначность числа
+				x = sc.nextInt();
+				if ((x<100) || (x>999)) System.out.print("\nВведите натуральное ТРЕХЗНАЧНОЕ число X:\t"); 
+			}
+			while ((x<100) || (x>999)); 
+			sc.close();
+			x = x/100+x%100/10+x%10;
+			System.out.println("\nРезультат =>\n\tсумма цифр числа X:\t" + x + "\n\n! Вычисления закончены !");
+	}
+}
